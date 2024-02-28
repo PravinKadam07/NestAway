251a7e2426850d1485cd82e2b2283005360a2d9e
Pravin Kadam <pravinkadam21998@gmail.com>
2024-02-28 20:06:10 +0530

    edit and update route

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/nestAway";
 
@@ -51,6 +53,19 @@ app.post("/listing", async (req, res) => {
   res.redirect("/listings");
 });
 
+//edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//edit update route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 // app.get("/testListing", async (req, res) => {
 //   let sample = new Listing({
 //     title: "My Villa",
