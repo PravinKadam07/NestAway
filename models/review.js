4a0a8df7ca2b4552f3e853e2d9972bc966a50781
Pravin Kadam <pravinkadam21998@gmail.com>
2024-03-01 17:29:47 +0530

    review model

---
+++ models/review.js
@@ -0,0 +1,18 @@
+const { string, number, date } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+  Comment: string,
+  rating: {
+    type: number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
