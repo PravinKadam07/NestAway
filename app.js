1f71dadc482d702b77879955a86fa938a282ada4
Pravin Kadam <pravinkadam21998@gmail.com>
2024-03-02 22:40:24 +0530

    User model + passport implementation

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -44,6 +47,10 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOption));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
